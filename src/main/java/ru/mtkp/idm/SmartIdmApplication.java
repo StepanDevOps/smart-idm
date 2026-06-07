26ca863206577ffa3e6a03fb7332559a6281c6e3
Stepan <stepanpeshkov101@gmail.com>
2026-06-07 19:08:28 +0300

    Initial commit

---
+++ src/main/java/ru/mtkp/idm/SmartIdmApplication.java
@@ -0,0 +1,13 @@
+package ru.mtkp.idm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mtkp/idm/SmartIdmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtkp.idm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartIdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
